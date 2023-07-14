da6f574c3edfe8c7a7c785fc0cb7847415c49cde
LAPTOP-G1SCCE12\Dart <dmitrypermyakov1990@gmail.com>
2023-07-14 13:22:44 +0500

    layout for mobile and desktop  client product list

--- client-part/src/app/app.module.ts
+++ client-part/src/app/app.module.ts
@@ -13,6 +13,8 @@ import { QuantitiComponent } from './quantiti/quantiti.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { MobileBackgroundLineComponent } from './mobile-background-line/mobile-background-line.component';
 import { ClientOrderComponent } from './client-order/client-order.component';
+import { ClientProductListComponent } from './client-product-list/client-product-list.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +28,9 @@ import { ClientOrderComponent } from './client-order/client-order.component';
     QuantitiComponent,
     NavBarComponent,
     MobileBackgroundLineComponent,
-    ClientOrderComponent
+    ClientOrderComponent,
+    ClientProductListComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
---
+++ client-part/src/app/client-product-list/client-product-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-product-list',
+  templateUrl: './client-product-list.component.html',
+  styleUrls: ['./client-product-list.component.css']
+})
+export class ClientProductListComponent {
+
+}
