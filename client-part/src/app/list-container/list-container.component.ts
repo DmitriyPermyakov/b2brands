7f05031b25649d57d29cd93c8a5d0454557b72ea
LAPTOP-G1SCCE12\Dart <dmitrypermyakov1990@gmail.com>
2023-11-28 13:59:23 +0500

    feat: Change link in admin-order component. Change label in mobile layout of list container

--- client-part/src/app/list-container/list-container.component.ts
+++ client-part/src/app/list-container/list-container.component.ts
@@ -5,6 +5,7 @@ import * as OrdersActions from '../store/actions/orders.actions'
 import * as OrdersSelectors from '../store/selectors/orders.selectors'
 import { ClientOrder } from '../interfaces/clientOrder.interface'
 import { Observable } from 'rxjs'
+import { IsMobileService } from '../services/is-mobile.service'
 
 @Component({
 	selector: 'app-list-container',
@@ -13,18 +14,27 @@ import { Observable } from 'rxjs'
 })
 export class ListContainerComponent implements OnInit {
 	public text: string = ''
+	public label: string = ''
+	public isMobile: boolean = false
 	public orders$: Observable<ClientOrder[]>
-	constructor(private activatedRoute: ActivatedRoute, private store: Store) {}
+	constructor(private activatedRoute: ActivatedRoute, private store: Store, private mobileService: IsMobileService) {
+		this.isMobile = mobileService.isMobile
+	}
 
 	ngOnInit(): void {
 		switch (this.activatedRoute.snapshot.routeConfig.path) {
 			case 'active-orders':
 				this.text = 'active'
+				this.label = 'Активные заказы:'
 				this.store.dispatch(OrdersActions.loadActiveOrders())
 				this.orders$ = this.store.pipe(select(OrdersSelectors.selectAllOrders))
 				break
 			case 'completed-orders':
 				this.text = 'completed'
+				this.label = 'Завершенный заказы:'
+				//TODO: переделать на завершенные заказы
+				this.store.dispatch(OrdersActions.loadActiveOrders())
+				this.orders$ = this.store.pipe(select(OrdersSelectors.selectAllOrders))
 				//dispatch completed
 				break
 			case 'products':
